0d99278029ab54fa277d195cbd56a8b0320c9e08
Mohammed Jalal Jaleh <jalaljaleh@gmail.com>
2022-10-13 12:45:29 +0330

    Update RequireChannel.cs

--- Dynastio.Bot/Interactions/Preconditions/RequireChannel.cs
+++ Dynastio.Bot/Interactions/Preconditions/RequireChannel.cs
@@ -1,17 +1,22 @@
+/*!
+ * Discord Precondition Require Channel v1 (https://jalaljaleh.github.io/)
+ * Copyright 2021-2022 Jalal Jaleh
+ * Licensed under Apache (https://github.com/jalaljaleh/Dynastio.Discord/blob/master/LICENSE.txt)
+ * Original (https://github.com/jalaljaleh/Dynastio.Discord/blob/master/Dynastio.Bot/Interactions/Preconditions/RequireChannel.cs)
+ */
+
+namespace Discord.Interactions
+{
 ﻿using Discord.Interactions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Dynastio.Bot;
 using Discord.WebSocket;
 using Discord;
-using Dynastio;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Discord.Interactions
-{
     public class RequireChannel : PreconditionAttribute
     {
         public ulong? ChannelId
