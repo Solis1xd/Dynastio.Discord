daafd09317e710d8c782b68431cf910ba2317512
Mohammed Jalal Jaleh <jalaljaleh@gmail.com>
2022-10-13 04:29:40 +0330

    Update

--- Dynastio.Bot/Interactions/Preconditions/RequireGuildRole.cs
+++ Dynastio.Bot/Interactions/Preconditions/RequireGuildRole.cs
@@ -1,15 +1,21 @@
+/*!
+ * Discord Precondition Require_Guild_Role  v1.1 (https://jalaljaleh.github.io/)
+ * Copyright 2021-2022 Jalal Jaleh
+ * Licensed under MIT (https://github.com/jalaljaleh/Dynastio.Discord/blob/master/LICENSE.txt)
+ * Original (https://github.com/jalaljaleh/Dynastio.Discord/blob/master/Dynastio.Bot/Interactions/Preconditions/RequireGuildRole.cs)
+ */
+
+namespace Discord.Interactions
+{
 ﻿using Discord.Interactions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Dynastio.Bot;
 using Discord.WebSocket;
 using Discord;
 
-namespace Discord.Interactions
-{
     public class RequireGuildRole : PreconditionAttribute
     {
         public ulong? GuildRole
@@ -32,21 +38,19 @@ namespace Discord.Interactions
      
         public override Task<PreconditionResult> CheckRequirementsAsync(IInteractionContext context, ICommandInfo commandInfo, IServiceProvider services)
         {
-            if(Program.IsDebug())
-                return Task.FromResult(PreconditionResult.FromSuccess());
-
+         
             var guild = context.Guild as SocketGuild;
             if (this.GuildId.HasValue) guild = (context.Client as DiscordSocketClient).Guilds.Where(a => a.Id == this.GuildId).FirstOrDefault();
 
-            if (guild == null) return Task.FromResult(PreconditionResult.FromError(this.ErrorMessage ?? "Guild Not Found."));
+            if (guild == null) return Task.FromResult(PreconditionResult.FromError("Guild Not Found."));
 
             var user = guild.GetUser(context.User.Id);
-            if (user == null) return Task.FromResult(PreconditionResult.FromError(this.ErrorMessage ?? "User Not Found."));
+            if (user == null) return Task.FromResult(PreconditionResult.FromError("User Not Found."));
 
             if (user.Roles.Where(r => r.Id == this.GuildRole).Any())
                 return Task.FromResult(PreconditionResult.FromSuccess());
 
-            return Task.FromResult(PreconditionResult.FromError(this.ErrorMessage ?? "You don't have permission."));
+            return Task.FromResult(PreconditionResult.FromError(this.ErrorMessage ?? "You need guild role."));
         }
     }
 }
